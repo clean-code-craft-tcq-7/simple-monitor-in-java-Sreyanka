169205cdf6d8d93e1185642f469e5a3b06531480
Sreyanka <35264560+Sreyanka@users.noreply.github.com>
2023-01-06 11:19:41 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -3,6 +3,5 @@ package vitals;
 public class Main {
     public static void main(String[] args) {
         BatteryTest.assertBMSParameters();
-        System.out.println("Some more tests needed");
     }
 }
